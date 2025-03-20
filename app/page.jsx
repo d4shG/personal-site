dc1347ef014fa0b4434934c9955c4b9afdb7227d
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-20 19:35:51 +0100

    base files added

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  );
+}
