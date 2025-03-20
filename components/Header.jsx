733e47633f8a88e3d6b21ed8d0f24b79e5804c2d
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-20 19:35:17 +0100

    header component and styles added

---
+++ components/Header.jsx
@@ -0,0 +1,60 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { LuBotMessageSquare } from "react-icons/lu";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
+
+const Header = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme === "light") {
+            setIsDarkMode(false);
+            document.body.classList.add("light-theme");
+        } else {
+            setIsDarkMode(true);
+            document.body.classList.remove("light-theme");
+        }
+    }, []);
+
+    const handleThemeToggle = () => {
+        setIsDarkMode(!isDarkMode);
+
+        if (isDarkMode) {
+            localStorage.setItem("theme", "light");
+            document.body.classList.remove("light-theme");
+        } else {
+            localStorage.setItem("theme", "dark");
+            document.body.classList.add("light-theme");
+        }
+    };
+
+    return (
+        <header className="header">
+            <div className="wrapper">
+                <div className="header-container">
+                    <div className="header-logo">
+                        <span>-G</span>
+                    </div>
+                    <div className="header-buttons">
+                        <div className="main-button">
+                            <Link href="#contact">
+                                <span>Contact me</span>
+                                <LuBotMessageSquare />
+                            </Link>
+                        </div>
+                        <div className="theme-button"
+                            onClick={handleThemeToggle}>
+                            {isDarkMode ?
+                                <IoMdMoon /> :
+                                <IoMdSunny />}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
