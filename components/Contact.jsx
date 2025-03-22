6b6cb45050601cbc6299ba24b4b04074e382356c
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-22 15:45:33 +0100

    Refactored styles

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,17 +1,21 @@
-"use client"
-import React, { useState } from 'react';
-import { FaUser, FaEnvelope, FaRegComments } from 'react-icons/fa';
-import emailjs from 'emailjs-com';
+"use client";
+import React, { useState } from "react";
+import { FaUser, FaEnvelope, FaRegComments } from "react-icons/fa";
+import emailjs from "emailjs-com";
+import Toast from "./Toast";
 
 const ContactForm = () => {
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: '',
-        timestamp: '',
+        name: "",
+        email: "",
+        message: "",
+        timestamp: "",
     });
     const [errors, setErrors] = useState({});
     const [submitted, setSubmitted] = useState(false);
+    const [toastMessage, setToastMessage] = useState("");
+    const [showToast, setShowToast] = useState(false);
+    const [toastType, setToastType] = useState("");
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -26,21 +30,21 @@ const ContactForm = () => {
         let isValid = true;
 
         if (!formData.name.trim()) {
-            formErrors.name = 'Name is required';
+            formErrors.name = "Name is required";
             isValid = false;
         }
 
         const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if (!formData.email.trim()) {
-            formErrors.email = 'Email is required';
+            formErrors.email = "Email is required";
             isValid = false;
         } else if (!emailPattern.test(formData.email)) {
-            formErrors.email = 'Invalid email format';
+            formErrors.email = "Invalid email format";
             isValid = false;
         }
 
         if (!formData.message.trim()) {
-            formErrors.message = 'Message is required';
+            formErrors.message = "Message is required";
             isValid = false;
         }
 
@@ -51,98 +55,106 @@ const ContactForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (validateForm()) {
-            // Add the timestamp when submitting
-            const updatedFormData = {
-                ...formData,
-                timestamp: new Date().toISOString(), // Set timestamp to current time
-            };
+        if (!validateForm())
+            return
 
-            emailjs .send(
+        const updatedFormData = {
+            ...formData,
+            timestamp: new Date().toISOString(),
+        };
+
+        emailjs
+            .send(
                 process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
                 process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-                formData,
+                updatedFormData,
                 process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
             )
             .then((response) => {
-                console.log('SUCCESS!', response);
+                console.log("SUCCESS!", response);
                 setSubmitted(true);
                 setFormData({
-                    name: '',
-                    email: '',
-                    message: '',
-                    timestamp: '',
+                    name: "",
+                    email: "",
+                    message: "",
+                    timestamp: "",
                 });
+                setToastMessage("Message sent successfully!");
+                setToastType("success");
+                setShowToast(true);
             })
             .catch((err) => {
-                console.error('FAILED...', err);
+                setToastMessage("Failed to send message. Please try again.");
+                setToastType("error");
+                setShowToast(true);
             });
-        }
+
     };
 
     return (
-        <section className='contact background-svg' id='contact' data-aos="fade-up">
+        <section className="contact background-svg" id="contact" data-aos="fade-up">
             <div className="contact-container">
                 <h2>Contact Us</h2>
-                {submitted ? (
-                    <div className="form-submitted">
-                        <h3>Thank you for your message!</h3>
+
+                <form onSubmit={handleSubmit} className="contact-form">
+                    <div className="form-group">
+                        <label htmlFor="name">
+                            <FaUser className="form-icon" />
+                            Name:
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="Your name"
+                        />
+                        {errors.name && <p className="error">{errors.name}</p>}
                     </div>
-                ) : (
-                    <form onSubmit={handleSubmit} className="contact-form">
-                        <div className="form-group">
-                            <label htmlFor="name">
-                                <FaUser className="form-icon" />
-                                Name:
-                            </label>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                placeholder="Your name"
-                            />
-                            {errors.name && <p className="error">{errors.name}</p>}
-                        </div>
 
-                        <div className="form-group">
-                            <label htmlFor="email">
-                                <FaEnvelope className="form-icon" />
-                                Email:
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                placeholder="Your email"
-                            />
-                            {errors.email && <p className="error">{errors.email}</p>}
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="email">
+                            <FaEnvelope className="form-icon" />
+                            Email:
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Your email"
+                        />
+                        {errors.email && <p className="error">{errors.email}</p>}
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor="message">
-                                <FaRegComments className="form-icon" />
-                                Message:
-                            </label>
-                            <textarea
-                                id="message"
-                                name="message"
-                                value={formData.message}
-                                onChange={handleChange}
-                                placeholder="Your message"
-                            />
-                            {errors.message && <p className="error">{errors.message}</p>}
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="message">
+                            <FaRegComments className="form-icon" />
+                            Message:
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            placeholder="Your message"
+                        />
+                        {errors.message && <p className="error">{errors.message}</p>}
+                    </div>
 
-                        <button type="submit">
-                            Submit
-                        </button>
-                    </form>
-                )}
+                    <button type="submit">Submit</button>
+                </form>
+
             </div>
+
+            <Toast
+                message={toastMessage}
+                show={showToast}
+                onClose={() => setShowToast(false)}
+                type={toastType}
+            />
         </section>
     );
 };
