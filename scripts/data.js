8a6a8056397923429bc50f21ca91993c8f086f09
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-03-22 15:56:08 +0100

    Refactored data to separate js

---
+++ scripts/data.js
@@ -0,0 +1,35 @@
+export const servicesData = [
+  {
+    category: 'IT Consulting',
+    services: [
+      'IT Planning & Infrastructure Setup',
+      'Database Management & Optimization',
+      'Data Recovery & System Troubleshooting',
+      'Web Administration & System Support',
+    ]
+  },
+  {
+    category: 'Developer Services',
+    services: [
+      'Custom Web Application Development',
+      'Full Stack Development',
+      'API Development & Integration',
+      'Database Design & Management',
+    ]
+  }
+];
+
+export const projects = [
+    {
+      title: "This site💟",
+      markdownFile: "https://raw.githubusercontent.com/gb4rt0s/personal-site/refs/heads/main/README.md",
+    },
+    {
+      title: "Aurum",
+      markdownFile: "https://raw.githubusercontent.com/Aurum-ElProyecteGrande/aurum/refs/heads/main/README.md",
+    },
+     {
+      title: "SolarWatch",
+      markdownFile: "https://raw.githubusercontent.com/gb4rt0s/solarwatch-ci/refs/heads/main/README.md",
+    },
+  ];
